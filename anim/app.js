05288feabda7b5df1a2ff0f79e06251a960c1bb3
Troy Betz <troybetz@gmail.com>
2014-02-02 19:35:43 -0600

    revise anim/app.js

--- anim/app.js
+++ anim/app.js
@@ -1,24 +1,21 @@
 (function() {
+
   function makeSmall(el) {
     var style = el.style;
     var currentWidth = window.getComputedStyle(el).width;
-
-    if (currentWidth === '0px') {
-      document.getElementById('toggle').innerHTML = 'hahaha'
-      style.width = '16.66%';
-    } else {
-      document.getElementById('toggle').innerHTML = 'Click me'
-      style.width = 0;
-    }
+    style.width = (currentWidth === '0px')
+      ? '16.66%'
+      : 0;
   }
+  
 
   var el = document.getElementById('toggle');
 
   el.addEventListener('click', function() {
     var list = document.getElementsByClassName('box');
-
     for (var i = 0; i < list.length; i++) {
       makeSmall(list[i]);
     }
-  })
+  });
+
 })();
